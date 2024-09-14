f1ba2cc3fbf68c404aee96142ed69dba70855bed
Swayam Bhalotia <swayambhalotia@gmail.com>
2024-09-14 22:54:13 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const handleUserSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/users', { name, email });
+      const response = await axios.post('https://task-35-ref-and-populate.vercel.app/users', { name, email });
       setName('');
       setEmail('');
       console.log(response.data);
@@ -24,7 +24,7 @@ function App() {
   const handlePostSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/posts', { title, content, email });
+      const response = await axios.post('https://task-35-ref-and-populate.vercel.app/posts', { title, content, email });
       setTitle('');
       setContent('');
       setEmail(''); 
@@ -38,7 +38,7 @@ function App() {
 
   const fetchPosts = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/posts');
+      const response = await axios.get('https://task-35-ref-and-populate.vercel.app/posts');
       console.log(response.data);
       setPosts(response.data);
     } catch (error) {
